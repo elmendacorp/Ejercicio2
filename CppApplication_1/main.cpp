be77457c1256d604e85b0295faedccd3be4953bf
Fran <Fran@DESKTOP-1Q3HOGC>
2016-03-07 17:26:18 +0100

    initalCommit

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Fran
+ *
+ * Created on 7 de marzo de 2016, 17:25
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
